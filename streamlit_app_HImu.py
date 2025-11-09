4a5ca80d91288e8ca69fc6414e6717dd5d616e3c
yashk468 <yashkhandelwal468@gmail.com>
2025-11-09 13:08:34 +0530

    Update streamlit_app_HImu.py

--- streamlit_app_HImu.py
+++ streamlit_app_HImu.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMKFVdkwmK0lwONyVIg79oy",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
